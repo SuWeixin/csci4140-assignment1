d20c5ad79493e03893373407a511dd2ebd9cd11a
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-15 16:08:24 +0800

    Update homepage

--- app/login.php
+++ app/login.php
@@ -1,8 +1,24 @@
 <?php
-// $db = parse_url(getenv("DATABASE_URL"));
-// echo "hello, db user";
+$db = parse_url(getenv("DATABASE_URL"));
+echo "hello, db user";
 
-if(isset($_POST['button'])){
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$name = $_POST['username'];
+$pass = $_POST['password'];
+
+$sql = "SELECT * FROM MyUser where name = '{$name}' and passwords = '{$pass}';"
+
+$stmt = $pdo->query($sql);
+
+if(isset($_POST['button']) && $stmt->rowcount() != 0){
     setcookie('username',$_POST['username'], time()+36000);
     header('location:homepage.php');
 }
