5c442a3761eef14aed8570783032feeeaad9c179
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-15 23:01:33 +0800

    Update

--- app/login.php
+++ app/login.php
@@ -15,20 +15,14 @@ $password = $_POST['password'];
 
 $sql = "SELECT * FROM MyUser where name = " + $name + " and passwords = " + $password + ";";
 
-$stmt = $pdo->query($sql);
-
-// $flag = false;
-// if ($_POST['username'] == 'admin' && $_POST['password'] == 'minda123') {
-//     $flag = true;
-// }
-// if ($_POST['username'] == 'Alice' && $_POST['password'] == 'csci4140') {
-//     $flag = true;
-// }
-
-if(isset($_POST['button']) && $stmt->fetchColumn() > 0){
-    setcookie('username',$_POST['username'], time()+36000);
-    header('location:homepage.php');
-} else {
-    header('location:index.php');
+if($stmt = $pdo->query($sql)) {
+    if($stmt->fetchColumn() > 0){
+        setcookie('username',$_POST['username'], time()+36000);
+        header('location:homepage.php');
+    } else {
+        header('location:index.php');
+    }
 }
+
+
 ?>
