3f888f46449093f9d00610231b4fdbf7d0650aff
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-15 11:59:58 +0800

    Update

--- app/index.php
+++ app/index.php
@@ -1,6 +1,26 @@
-<?php
+<!-- <?php
     if(isset($_COOKIE['username'])) {
         echo 'You are logged as'.$_COOKIE['username'].'</br>';
         echo '<a href="logout.php" Log Out </a>';
     }
-?>
+?> -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Web Instagram</h2>
+    <form id="loginform" name="loginform" method="post" action="login.php">
+        Username:<br>
+        <input type="text" name="username" id="username">
+        <br>
+        Password:<br>
+        <input type="password" name="password" id="password">
+        <br>
+        <input type="submit" name="button" id="button" value="Login">
+    </form>
+</body>
+</html>
