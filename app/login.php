4d5f62aeafd9690bb08e6f8a517b3ab761ba416c
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-15 21:23:08 +0800

    Update

--- app/login.php
+++ app/login.php
@@ -1,23 +1,23 @@
 <?php
-// $db = parse_url(getenv("DATABASE_URL"));
+$db = parse_url(getenv("DATABASE_URL"));
 
-// $pdo = new PDO("pgsql:" . sprintf(
-//     "host=%s;port=%s;password=%s;dbname=%s",
-//     $db["host"],
-//     $db["port"],
-//     $db["user"],
-//     $db["pass"],
-//     ltrim($db["path"], "/")
-// ));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["name"],
+    $db["passwords"],
+    ltrim($db["path"], "/")
+));
 
-// $name = $_POST['username'];
-// $pass = $_POST['password'];
+$name = $_POST['username'];
+$pass = $_POST['password'];
 
-// $sql = "SELECT * FROM MyUser where name = '{$name}' and passwords = '{$pass}';"
+$sql = "SELECT * FROM MyUser where name = '{$name}' and passwords = '{$pass}';"
 
-// $stmt = $pdo->query($sql);
+$stmt = $pdo->query($sql);
 
-if(isset($_POST['button'])){
+if(isset($_POST['button']) && $stmt->rowcount() != 0){
     setcookie('username',$_POST['username'], time()+36000);
     header('location:homepage.php');
 }
