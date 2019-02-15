3bf5cd125fb56eac0587bac722393e934cbf6da1
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-16 00:56:31 +0800

    Upload

--- app/homepage.php
+++ app/homepage.php
@@ -1,4 +1,12 @@
-
+<?php
+require('$_SERVER['DOCUMENT_ROOT']./vendor/autoload.php');
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = new Aws\S3\S3Client([
+    'version'  => '2006-03-01',
+    'region'   => 'us-east-1',
+]);
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+?>
 <html>
 <body>
 <?php
@@ -8,15 +16,7 @@
     }
 ?>
 
-<p>
-    Picture Upload
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-</p>
-<h1>S3 upload example</h1>
+<h1>Image upload</h1>
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
