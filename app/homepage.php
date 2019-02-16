6f2b165a114efc84270faf6c012dba6b3a5abeaf
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-16 16:53:07 +0800

    Update

--- app/homepage.php
+++ app/homepage.php
@@ -1,12 +1,3 @@
-<?php
-include('vendor/autoload.php');
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = new Aws\S3\S3Client([
-    'version'  => '2006-03-01',
-    'region'   => 'us-east-1',
-]);
-// $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-?>
 <html>
 <body>
 <?php
@@ -16,21 +7,38 @@ $s3 = new Aws\S3\S3Client([
     }
 ?>
 
-<h1>Image upload</h1>
+<h3>User Image</h3>
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-    // FIXME: add more validation, e.g. using ext/fileinfo
-    try {
-        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+    $db = parse_url(getenv("DATABASE_URL"));
+
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+
+    $username = $_COOKIE['username'];
+
+    $sql = "SELECT image from USERIMAGES WHERE name = '{$username}' ORDER BY ts DESC;";
+
+    foreach ($pdo->query($sql) as $row) {
+        echo $row['image'];
+    }
 ?>
-        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
-<?php } catch(Exception $e) { ?>
-        <p>Upload error :(</p>
-<?php } } ?>
-        <h2>Upload a file</h2>
-        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-            <input name="userfile" type="file"><input type="submit" value="Upload">
-        </form>
+
+<h3>Image upload</h3>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <select name="private">
+        <option value="private">Private</option>
+        <option value="public">Public</option>
+    </select>
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
 </body>
 </html>
---
+++ app/upload.php
@@ -0,0 +1,36 @@
+<?php
+if (isset($_POST['submit'])) {
+    $image_username = $_COOKIE['username'];
+    $image_data_name = $_FILES['fileToUpload']['name'];
+    $image_data_size = $_FILES['fileToUpload']['size'];
+    $image_data_type = $_FILES['fileToUpload']['type'];
+    $image_data = $_FILES['fileToUpload']['tmp_name'];
+    $image_upload_mode = 
+
+    if ($image_data_type != 'image/jpeg' && $image_data_type != 'image/gif' && $image_data_type != 'image/png') {
+        header('location:homepage.php');
+    } else {
+        $db = parse_url(getenv("DATABASE_URL"));
+
+        $pdo = new PDO("pgsql:" . sprintf(
+            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+            $db["host"],
+            $db["port"],
+            $db["user"],
+            $db["pass"],
+            ltrim($db["path"], "/")
+        ));
+
+        $sql1 = "SELECT * FROM USERIMAGES;";
+        $stmt1 = $pdo->query($sql1);
+        $image_id = $stmt1->fetchColumn();
+
+        $data = addslashes(fread(fopen($image_data, "r"), filesize($image_data_size)));
+        $sql2 = "INSERT INTO UMERIMAGES (id, username, type, image, ts) VALUES 
+                ({$image_id}, '{$image_username}', '{$image_data_type}', '{$data}', NOW());";
+        if ($pdo->query($sql2)) {
+            header('location:homepage.php');
+        }     
+    }
+}
+?>
