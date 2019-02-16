232566e4ca1da8d0f9dfe53439933fa39bd70336
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-02-16 23:26:42 +0800

    Upload

--- app/index.php
+++ app/index.php
@@ -16,19 +16,6 @@
         <br>
         <input type="submit" name="button" id="button" value="Login">
     </form>
-    
-<?php
-require('../vendor/autoload.php');
-header('Content-type: image/jpeg');
-
-
-$image_in = new Imagick('test.jpg');
-
-$image_in->blurImage(10,10);
-$image_in->borderImage('black', 100, 100);
-
-echo $image_in;
-?>
 
 </body>
 </html>
